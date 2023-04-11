117fc7b80d50b82354366c6d437772903dc1e758
lucsbandeira06 <lucasbandeira06@gmail.com>
2023-04-11 02:21:01 +0100

    index.html

--- app.py
+++ app.py
@@ -1,13 +1,38 @@
 from flask import Flask, render_template, request, url_for, redirect
-from pymongo import MongoClient
+from bson.objectid import ObjectId
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-client = MongoClient('localhost', 27017)
+app.config['MONGO_URI'] = 'mongodb+srv://lucasbandeira06:Lucas6705229@cluster0.jxvdd.mongodb.net/mydatabase?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
-db = client.flask_db
-todos = db.todos
+todos = mongo.db.todos
 
-@app.route('/', methods=('GET', 'POST'))
+@app.route('/')
 def index():
-    return render_template('index.html')
+    saved_todos = todos.find()
+    return render_template('index.html', todos=saved_todos)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    new_todo = request.form.get('new-todo')
+    todos.insert_one({'text' : new_todo, 'complete' : False})
+    return redirect(url_for('index'))
+
+@app.route('/complete/<oid>')
+def complete(oid):
+    todo_item = todos.find_one({'_id': ObjectId(oid)})
+    todo_item['complete'] = True
+    todos.save(todo_item)
+    return redirect(url_for('index'))
+
+@app.route('/delete_completed')
+def delete_completed():
+    todos.delete_many({'complete' : True})
+    return redirect(url_for('index'))
+
+@app.route('/delete_all')
+def delete_all():
+    todos.delete_many({})
+    return redirect(url_for('index'))
